02d39fce6e2419252b1288fd26bc5c44f700aa33
nunomota <lostintime@nunomota.io>
2017-10-21 19:05:46 +0200

    Completes cross validation implementation

--- ex04/template/ridge_regression.py
+++ ex04/template/ridge_regression.py
@@ -8,7 +8,5 @@ def ridge_regression(y, tx, lambda_):
     I = np.eye(tx.shape[1])
     inv = np.linalg.inv(tx.T.dot(tx) + 2 * tx.shape[1] * lambda_ * I)
     w_opt = inv.dot(tx.T).dot(y)
-    #rmse = np.sqrt(compute_mse(y, tx, w_opt) +
-    #    lambda_ * np.linalg.norm(w_opt)**2)
     rmse = np.sqrt(compute_mse(y, tx, w_opt))
     return w_opt, rmse
