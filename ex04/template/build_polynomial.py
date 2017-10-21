d87f69eec5a004f4992b8bac6960ee5478f0e536
nunomota <lostintime@nunomota.io>
2017-10-21 15:55:48 +0200

    Adds implementation for auxiliary functions

--- ex04/template/build_polynomial.py
+++ ex04/template/build_polynomial.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-"""implement a polynomial basis function."""
-
 import numpy as np
 
 
-def build_poly(x, degree):
-    """polynomial basis functions for input data x, for j=0 up to j=degree."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # polynomial basis function: TODO
-    # this function should return the matrix formed
-    # by applying the polynomial basis to the input data
-    # ***************************************************
-    raise NotImplementedError
+def build_poly(x, degree, offset=True):
+    '''
+    Polynomial basis functions for input data x,
+    for up to a certain degree.
+    '''
+    if offset:
+        rows, cols = np.indices((x.shape[0], degree+1))
+        tx = np.power(x[rows], cols)
+    else:
+        rows, cols = np.indices((x.shape[0], degree))
+        tx = np.power(x[rows], cols+1)
+    return tx
--- ex04/template/ridge_regression.py
+++ ex04/template/ridge_regression.py
@@ -1,16 +1,15 @@
 # -*- coding: utf-8 -*-
-"""Exercise 3.
-
-Ridge Regression
-"""
-
 import numpy as np
 
 
 def ridge_regression(y, tx, lambda_):
-    """implement ridge regression."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # ridge regression: TODO
-    # ***************************************************
-    raise NotImplementedError
+    '''
+    Implements ridge regression using normal equations.
+    '''
+    I = np.eye(tx.shape[1])
+    inv = np.linalg.inv(tx.T.dot(tx) + 2 * tx.shape[1] * lambda_ * I)
+    w_opt = inv.dot(tx.T).dot(y)
+    #rmse = np.sqrt(compute_mse(y, tx, w_opt) +
+    #    lambda_ * np.linalg.norm(w_opt)**2)
+    rmse = np.sqrt(compute_mse(y, tx, w_opt))
+    return w_opt, rmse
