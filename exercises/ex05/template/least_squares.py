f3a04002f6b0d24753ad9ca94a533b1f43ba6b63
Lucia <lumosan02@gmail.com>
2017-10-23 10:11:04 +0200

    Work on ex05

--- exercises/ex05/template/least_squares.py
+++ exercises/ex05/template/least_squares.py
@@ -9,9 +9,18 @@ import numpy as np
 
 def least_squares(y, tx):
     """calculate the least squares."""
-    # ***************************************************
-    # INSERT YOUR CODE HERE
-    # least squares: TODO
-    # returns mse, and optimal weights
-    # ***************************************************
-    raise NotImplementedError
+    w_opt = np.linalg.pinv(tx).dot(y)
+    rmse = np.sqrt(compute_mse(y, tx, w_opt))
+    return w_opt, rmse
+
+
+def compute_mse(y, tx, w):
+    """Calculates the loss using MSE."""
+    if len(tx.shape) == 1:
+        tx = tx.reshape(-1, 1)
+    if len(y.shape) == 1:
+        y = y.reshape(-1, 1)
+    w = np.array(w).reshape(tx.shape[1], 1)
+    z = y - tx.dot(w)
+    z = z.T.dot(z)
+    return z[0][0] / tx.shape[0]
